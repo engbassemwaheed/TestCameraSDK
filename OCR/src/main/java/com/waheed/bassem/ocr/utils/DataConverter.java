54308aaa9f06d6533f34cd56150ede20c070ec20
Bassem Waheed <eng.bassem.waheed@gmail.com>
2022-01-15 14:45:30 +0200

    - edited image quality that will be uploaded to the server

--- OCR/src/main/java/com/waheed/bassem/ocr/utils/DataConverter.java
+++ OCR/src/main/java/com/waheed/bassem/ocr/utils/DataConverter.java
@@ -12,7 +12,7 @@ public class DataConverter {
 
     public static String imageToBase64 (Bitmap bitmap) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream); //bm is the bitmap object
+        bitmap.compress(Bitmap.CompressFormat.PNG, 50, byteArrayOutputStream); //bm is the bitmap object
         byte[] byteArrayImage = byteArrayOutputStream.toByteArray();
         String base64String = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
         String base64Fixed = base64String.replace("\n", "");
